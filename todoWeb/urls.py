c8695771be1533861b6b5b50ac0761e3ff05ef58
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-03-12 13:15:33 +0530

    routing done

--- todoWeb/urls.py
+++ todoWeb/urls.py
@@ -7,6 +7,7 @@ app_name="todo"
 
 urlpatterns = [
     path("",views.task_list,name='task_list'),
+    path("add/",views.add_task,name="add_task"),
     path("edit/<int:id>",views.edit_task,name='edit_task'),
     path("delete/<int:id>",views.delete_task,name='delete_task'),
     path("toggle<int:id>",views.toggle_task,name='toggle_task')
