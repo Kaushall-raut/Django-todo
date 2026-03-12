ce3374105dee44e33cbf38ee6fe780da1f24c810
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-03-12 13:14:05 +0530

    routing done

--- todo/urls.py
+++ todo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('todoWeb.urls'))
+    path("",include('todoWeb.urls'), name="todo")
 ]
---
+++ todoWeb/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+
+app_name="todo"
+
+
+urlpatterns = [
+    path("",views.task_list,name='task_list'),
+    path("edit/<int:id>",views.edit_task,name='edit_task'),
+    path("delete/<int:id>",views.delete_task,name='delete_task'),
+    path("toggle<int:id>",views.toggle_task,name='toggle_task')
+]
